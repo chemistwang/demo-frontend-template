7016e408b266b1ef7db1db0b51df1ce50fb5ff1d
chemputer <chemputer_dev@163.com>
2022-11-24 14:30:38 +0800

    ci: add eslint and commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        'standard',
+        'eslint-config-standard-with-typescript',
+        'prettier'
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "prettier"
+    ],
+    "rules": {
+        'prettier/prettier': 'error'
+    }
+}
